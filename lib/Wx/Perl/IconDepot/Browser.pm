c4de421d1b161b7caaf0c7317cb5fed381227ccd
Hans Jeuken <hansjeuken@xs4all.nl>
2017-11-04 16:20:28 +0100

    modified:   Changes 	modified:   MANIFEST 	modified:   bin/iconbrowser 	modified:   lib/Wx/Perl/IconDepot.pm 	modified:   lib/Wx/Perl/IconDepot/Browser.pm 	modified:   lib/Wx/Perl/IconDepot/Viewer.pm 	new file:   lib/Wx/Perl/IconDepot/image_missing.png 	modified:   t/Wx-Perl-IconDepot.t

--- lib/Wx/Perl/IconDepot/Browser.pm
+++ lib/Wx/Perl/IconDepot/Browser.pm
@@ -1,16 +1,92 @@
 package Wx::Perl::IconDepot::Browser;
 
+=head1 NAME
+
+Wx::Perl::IconDepot::Browser - Browse icon libraries quick & easy
+
+=cut
+
+use strict;
+use warnings;
+
 use Wx qw( :frame :sizer :panel :window :id);
 use base qw( Wx::Panel );
+use Wx::Perl::IconDepot::Viewer;
+
+###############################################################################
+=head1 SYNOPSIS
+
+=over 4
+
+
+=back
+
+=head1 DESCRIPTION
+
+
+=cut
 
 sub new {
-    my $class = shift;
-    my $self = $class->SUPER::new(@_);
-    my $sizer = Wx::BoxSizer->new(wxVERTICAL);
-    $self->SetSizer($sizer);
+	my $class = shift;
+	my $self = $class->SUPER::new(@_);
+	my $sizer = Wx::BoxSizer->new(wxVERTICAL);
+	$self->SetSizer($sizer);
 
-    return $self;
+	my $navigator = Wx::Panel->new($self, -1);
+	$sizer->Add($navigator, 0, wxEXPAND);
+	my $nsiz = Wx::BoxSizer->new(wxHORIZONTAL);
+	$navigator->SetSizer($nsiz);
+	
+	my $ttext = Wx::StaticText->new($navigator, -1, 'Theme:');
+	$nsiz->Add($ttext, 0, wxALIGN_CENTER_VERTICAL|wxALL, 2 );
+	my $tcom = Wx::ComboBox->new($navigator, -1);
+	$nsiz->Add($tcom, 1, wxALL, 2);
+
+	my $stext = Wx::StaticText->new($navigator, -1, 'Size:');
+	$nsiz->Add($stext, 0, wxALIGN_CENTER_VERTICAL|wxALL, 2  );
+	my $scom = Wx::ComboBox->new($navigator, -1);
+	$nsiz->Add($scom, 1, wxALL, 2);
+
+	my $ctext = Wx::StaticText->new($navigator, -1, 'Context:');
+	$nsiz->Add($ctext, 0, wxALIGN_CENTER_VERTICAL|wxALL, 2  );
+	my $ccom = Wx::ComboBox->new($navigator, -1);
+	$nsiz->Add($ccom, 1, wxALL, 2);
+
+
+	my $viewer = Wx::Perl::IconDepot::Viewer->new($self, -1);
+	$sizer->Add($viewer, 1, wxEXPAND);
+	$self->Layout;
+	$self->Fit;
+	return $self;
 }
+
+###############################################################################
+=back
+
+=head1 PRIVATE METHODS
+
+=over 4
+
+=cut
+
+
+###############################################################################
+=back
+
+=head1 AUTHOR
+
+Hans Jeuken (hansjeuken at xs4all dot nl)
+
+=head1 BUGS
+
+If you find any, please contact the author.
+
+Icon libararies that depend on .svg images show up in the list of 
+B<AvailableThemes> when no support for scalable vector graphics is available.
+
+=head1 TODO
+
+=cut
 
 1;
 __END__
--- lib/Wx/Perl/IconDepot/Viewer.pm
+++ lib/Wx/Perl/IconDepot/Viewer.pm
@@ -1,13 +1,67 @@
-package Wx::Perl::IconDepot::Browser;
+package Wx::Perl::IconDepot::Viewer;
+
+=head1 NAME
+
+Wx::Perl::IconDepot::Viewer - Display and select icons on a panel
+
+=cut
 
 use Wx qw( :frame :sizer :panel :window :id);
-use base qw( Wx::Panel );
+use base qw( Wx::ScrolledWindow );
+
+###############################################################################
+=head1 SYNOPSIS
+
+=over 4
+
+
+=back
+
+=head1 DESCRIPTION
+
+
+=cut
 
 sub new {
     my $class = shift;
     my $self = $class->SUPER::new(@_);
     return $self;
 }
+
+=head1 PUBLIC METHODS
+
+=over 4
+
+=cut
+
+
+###############################################################################
+=back
+
+=head1 PRIVATE METHODS
+
+=over 4
+
+=cut
+
+
+###############################################################################
+=back
+
+=head1 AUTHOR
+
+Hans Jeuken (hansjeuken at xs4all dot nl)
+
+=head1 BUGS
+
+If you find any, please contact the author.
+
+Icon libararies that depend on .svg images show up in the list of 
+B<AvailableThemes> when no support for scalable vector graphics is available.
+
+=head1 TODO
+
+=cut
 
 1;
 __END__
